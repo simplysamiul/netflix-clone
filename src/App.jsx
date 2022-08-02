ded062156ce57e676b4333d94604bb95c731f413
Md Samiul <simplysamiul@gmail.com>
2022-08-02 22:21:05 +0600

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,17 @@
-import "./app.scss";
-import Home from "./pages/home/Home";
-import Register from "./pages/register/Register";
-import Watch from "./pages/watch/Watch";
-import Login from "./pages/login/Login";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "./authContext/AuthContext";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './app.scss';
+import Home from './home/Home';
 
 const App = () => {
-  const { user } = useContext(AuthContext);
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          {user ? <Home /> : <Redirect to="/register" />}
-        </Route>
-        <Route path="/register">
-          {!user ? <Register /> : <Redirect to="/" />}
-        </Route>
-        <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
-        {user && (
-          <>
-            <Route path="/movies">
-              <Home type="movie" />
-            </Route>
-            <Route path="/series">
-              <Home type="series" />
-            </Route>
-            <Route path="/watch">
-              <Watch />
-            </Route>
-          </>
-        )}
-      </Switch>
-    </Router>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element = {<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,57 +1,43 @@
-import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
-import { useContext, useState } from "react";
-import "./navbar.scss";
-import { Link } from "react-router-dom";
-import { AuthContext } from "../../authContext/AuthContext";
-import { logout } from "../../authContext/AuthAction";
+import netflix_logo from '../../assets/img/Netflix_logo.png';
+import { Link } from "react-router-dom"
+import {Search, Notifications, ArrowDropDown} from '@material-ui/icons';
+import profile_logo from '../../assets/img/profile.jpeg';
+import { useState } from 'react';
+import './navbar.scss';
 
 const Navbar = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-  const { dispatch } = useContext(AuthContext);
-
-  window.onscroll = () => {
-    setIsScrolled(window.pageYOffset === 0 ? false : true);
-    return () => (window.onscroll = null);
-  };
-  return (
-    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
-      <div className="container">
-        <div className="left">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-            alt=""
-          />
-          <Link to="/" className="link">
-            <span>Homepage</span>
-          </Link>
-          <Link to="/series" className="link">
-            <span className="navbarmainLinks">Series</span>
-          </Link>
-          <Link to="/movies" className="link">
-            <span className="navbarmainLinks">Movies</span>
-          </Link>
-          <span>New and Popular</span>
-          <span>My List</span>
-        </div>
-        <div className="right">
-          <Search className="icon" />
-          <span>KID</span>
-          <Notifications className="icon" />
-          <img
-            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-          />
-          <div className="profile">
-            <ArrowDropDown className="icon" />
-            <div className="options">
-              <span>Settings</span>
-              <span onClick={() => dispatch(logout())}>Logout</span>
+    const [isScrolled, setIsScrolled] = useState(false);
+    window.onscroll = () =>{
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => (window.onscroll = null);
+    }
+    return (
+        <div className="navbar">
+            <div className="container">
+                <div className="left">
+                    <img src={netflix_logo} alt="" />
+                    <Link to="/">HomePgae</Link>
+                    <Link to="/">Series</Link>
+                    <Link to="/">Mo0vies</Link>
+                    <Link to="/">New and Popular</Link>
+                    <Link to="/">My List</Link>
+                </div>
+                <div className="right">
+                    <Search className='icon' />
+                    <Link to="/">KID</Link>
+                    <Notifications className='icon' />
+                    <img src={profile_logo} alt="" />
+                    <div className="profile">
+                        <ArrowDropDown className='icon' />
+                        <div className="options">
+                            <Link to="/">Settings</Link>
+                            <Link to="/">Logout</Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
